aba1b4e411ddee12c9a96cc1037dbd9e2ad0c145
firatike <firratt.1@hotmail.com>
2021-03-08 23:18:22 +0300

    08.03

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+using Core.Entities.Concrete;
+using Entities.Concrete;
+
+namespace Business.Constants
+{
+    public static class Messages 
+    {
+        public static string ProductAdded = "Ürün Eklendi"; 
+        public static string ProductNameInvalid = "Ürün ismi geçersiz.";
+        public static string MaintenanceTime="Sistem Bakımda";
+        public static string ProductsListed="Ürünler Listelendi";
+
+        public static string ProductCountOfCategoryError = "Bir Kategoride en fazla 10 ürün olabilir";
+        public static string ProductNameAlreadyExists = "Bu isimde zaten başka bir ürün var";
+        public static string CategoryLimitExceted = "Kategori Limiti Aşıldığı için yeni ürün eklenemiyor";
+
+        public static string AuthorizationDenied="Yetkiniz yok";
+        public static string UserRegistered="Kayıt Oldu";
+        public static string UserNotFound="Kullanıcı Bulunamadı";
+        public static string PasswordError="Parola Hatası";
+        public static string SuccessfulLogin="Başarılı Giriş";
+        public static string UserAlreadyExists="Kullanıcı Mevcut";
+        public static string AccessTokenCreated="Token Oluşturuldu";
+    }
+}
---
+++ Core/Aspects/Autofac/Performance/PerformanceAspect.cs
@@ -0,0 +1,39 @@
+﻿using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
+using Core.Utilities.IoC;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Core.Aspects.Autofac.Performance
+{
+    public class PerformanceAspect : MethodInterception
+    {
+        private int _interval;
+        private Stopwatch _stopwatch; 
+
+        public PerformanceAspect(int interval)
+        {
+            _interval = interval;
+            _stopwatch = ServiceTool.ServiceProvider.GetService<Stopwatch>();
+        }
+
+
+        protected override void OnBefore(IInvocation invocation) //metodun önünde kronometreyi başlatıyoruz
+        {
+            _stopwatch.Start();
+        }
+
+        protected override void OnAfter(IInvocation invocation) //metot bittiğindede
+        {
+            if (_stopwatch.Elapsed.TotalSeconds > _interval) //o ana kadar geçen süre 5 saniyeden büyükse
+            {
+                Debug.WriteLine($"Performance : {invocation.Method.DeclaringType.FullName}.{invocation.Method.Name}-->{_stopwatch.Elapsed.TotalSeconds}");
+                //Burda tabiki console log olarak yazmış Engin Hoca. Biz burda artık mailmi göndeririz, logmu alırız o bizlerin bileceği iş
+            }
+            _stopwatch.Reset();
+        }
+    }
+}
